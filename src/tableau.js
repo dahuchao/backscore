15a3a539b29d9cd0b285de30e5589cccbcb031c8
GRANDIERE Emmanuel <pwzf831@corp.log.intra.laposte.fr>
2016-02-15 13:47:05 +0100

    Correction raffraichissement tableau suite sélection autre rencontre et récupération données de la rencontre sur le serveur.

--- src/tableau.js
+++ src/tableau.js
@@ -6,31 +6,37 @@ var socket = require('socket.io-client')('http://localhost')
 var Tableau = React.createClass({
   getInitialState: function() {
     console.info("Taleau de la rencontre " + JSON.stringify(this.props.rencontre))
-    let idRencontre = this.props.rencontre;
+    var rencontreCourante = {
+      rencontre:{
+        "id": 0,
+        "hote": {
+          "nom": "hote",
+          "marque": 1
+        },
+        "visiteur": {
+          "nom": "visiteur",
+          "marque": 1
+        }
+      }
+    }
     socket.on('connect', function() {
       console.info("Connecté avec la table de marque");
-      socket.emit('ouvrirRencontre', idRencontre);
     })
     socket.on('fournitureRencontre', this.surPremiereMarque)
     socket.on('nouvelleMarque', this.surNouvelleMarque)
-    return {
-      "id": 1,
-      "hote": {
-        "nom": "hote",
-        "marque": 1
-      },
-      "visiteur": {
-        "nom": "visiteur",
-        "marque": 1
-      }
-    }
+    return rencontreCourante
+  },
+  connexionTableMarque: function(idRencontre) {
+    console.info("Connexion à la table de marque" + JSON.stringify(idRencontre));
+    socket.emit('ouvrirRencontre', idRencontre);
   },
   surPremiereMarque: function(rencontre) {
     console.info("Reception première marque" + JSON.stringify(rencontre));
-    this.state.hote.nom = rencontre.hote.nom
-    this.state.hote.marque = rencontre.hote.marque
-    this.state.visiteur.nom = rencontre.visiteur.nom
-    this.state.visiteur.marque = rencontre.visiteur.marque
+    this.state.rencontre = rencontre
+    //this.state.rencontre.hote.nom = rencontre.hote.nom
+    //this.state.rencontre.hote.marque = rencontre.hote.marque
+    //this.state.rencontre.visiteur.nom = rencontre.visiteur.nom
+    //this.state.rencontre.visiteur.marque = rencontre.visiteur.marque
     this.setState(this.state)
     console.info("-Marque:" + JSON.stringify(this.state))
   },
@@ -38,41 +44,47 @@ var Tableau = React.createClass({
     console.info("Reception d'une nouvelle marque: " + + JSON.stringify(marque))
   },
   surPanierHote: function() {
-    console.info("Panier marque: " + JSON.stringify(this.state.hote.marque))
-    let marque = this.state.hote.marque
-    this.state.hote.marque = marque + 1
+    console.info("Panier marque: " + JSON.stringify(this.state.rencontre.hote.marque))
+    let marque = this.state.rencontre.hote.marque
+    this.state.rencontre.hote.marque = marque + 1
     this.setState(this.state)
-    socket.emit('panierMarque', this.state.hote.marque);
-    console.info("-Marque: " + JSON.stringify(this.state.hote.marque))
+    socket.emit('panierMarque', this.state.rencontre.hote.marque);
+    console.info("-Marque: " + JSON.stringify(this.state.rencontre.hote.marque))
   },
   surPanierVisiteur: function() {
-    console.info("Panier marque: " + JSON.stringify(this.state.visiteur.marque))
-    let marque = this.state.visiteur.marque
-    this.state.visiteur.marque = marque + 1
+    console.info("Panier marque: " + JSON.stringify(this.state.rencontre.visiteur.marque))
+    let marque = this.state.rencontre.visiteur.marque
+    this.state.rencontre.visiteur.marque = marque + 1
     this.setState(this.state)
-    console.info("-Marque: " + JSON.stringify(this.state.visiteur.marque))
+    console.info("-Marque: " + JSON.stringify(this.state.rencontre.visiteur.marque))
   },
   surCorrectionHote: function() {
     console.info("Correction de la marque");
-    let marque = this.state.hote.marque
-    this.state.hote.marque = marque - 1
+    let marque = this.state.rencontre.hote.marque
+    this.state.rencontre.hote.marque = marque - 1
     this.setState(this.state)
-    socket.emit('panierMarque', this.state.hote.marque);
-    console.info("-Marque: " + JSON.stringify(this.state.hote.marque))
+    socket.emit('panierMarque', this.state.rencontre.hote.marque);
+    console.info("-Marque: " + JSON.stringify(this.state.rencontre.hote.marque))
   },
   surCorrectionVisiteur: function() {
     console.info("Correction de la marque");
-    let marque = this.state.visiteur.marque
-    this.state.visiteur.marque = marque - 1
+    let marque = this.state.rencontre.visiteur.marque
+    this.state.rencontre.visiteur.marque = marque - 1
     this.setState(this.state)
-    console.info("-Marque: " + JSON.stringify(this.state.visiteur.marque))
+    console.info("-Marque: " + JSON.stringify(this.state.rencontre.visiteur.marque))
   },
   render: function() {
+    // Si la rencontre a changé
+    console.info("if: "+ JSON.stringify(this.props.rencontre)+"=" + JSON.stringify(this.state.rencontre.id))
+    if (this.props.rencontre != this.state.rencontre.id)
+    {
+      this.connexionTableMarque(this.props.rencontre)
+    }
     return (
       <div id="tableau">
-        <Equipe nom={this.state.hote.nom} surPanier={this.surPanierHote}/>
-        <Marque hote={this.state.hote.marque} visiteur={this.state.visiteur.marque} surCorrectionHote={this.surCorrectionHote} surCorrectionVisiteur={this.surCorrectionVisiteur}/>
-        <Equipe nom={this.state.visiteur.nom} surPanier={this.props.surPanierVisiteur}/>
+        <Equipe nom={this.state.rencontre.hote.nom} surPanier={this.surPanierHote}/>
+        <Marque hote={this.state.rencontre.hote.marque} visiteur={this.state.rencontre.visiteur.marque} surCorrectionHote={this.surCorrectionHote} surCorrectionVisiteur={this.surCorrectionVisiteur}/>
+        <Equipe nom={this.state.rencontre.visiteur.nom} surPanier={this.props.surPanierVisiteur}/>
       </div>
     )
   }
