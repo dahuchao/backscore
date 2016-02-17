5090f527e8a85e98d1ddfdd9138b2c413793b34f
GRANDIERE Emmanuel <pwzf831@corp.log.intra.laposte.fr>
2016-02-17 17:05:56 +0100

    Intégration Heroku

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var exec = require('gulp-exec');
 var sass = require('gulp-sass');
 var browserify = require('browserify');
 var babelify = require('babelify');
@@ -8,8 +9,6 @@ var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 var server = require('gulp-express');
 
-// Tache pour controler l'execution de gulp dans Atom
-gulp.task('stop', function() {});
 
 gulp.task('styles', function() {
   gulp.src('sass/**/*.scss')
@@ -31,38 +30,46 @@ gulp.task('fabrique', function() {
     .bundle()
     .on('error', gutil.log)
     .pipe(source('app.js'))
-    .pipe(gulp.dest('public'))
-    .pipe(reload({
-      stream: true
-    }));
+    .pipe(gulp.dest('public'));
+});
+
+
+// Convertit es6 en es5 et assemble les morceaux
+gulp.task('charger', function() {
+  exec('node util/util.js', function(err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
 });
 
 // Refabrique automatiquement sur tout Chargement des sources
 gulp.task('dev', ['fabrique', 'styles'], function() {
   // Serve files from the root of this project
   browserSync.init({
+    //serveStatic: ["public/"],
     server: {
       baseDir: "public/"
-    }
+    },
+    // Tells BrowserSync on where the express app is running
+    //proxy: 'http://localhost:80',
+    browser: "google chrome"
   });
   gulp.watch(['*.html', 'app.js', 'src/*.js'], ['fabrique'])
-  gulp.watch('sass/**/*.scss', ['styles']);
+  gulp.watch('sass/**/*.scss', ['styles'])
+  gulp.watch("public/**").on('change', browserSync.reload);
   server.run(['serveur.js']);
 });
 
-gulp.task('start', ['styles'], function() {
-  browserify({
-      entries: 'src/index.js',
-      debug: true
-    })
-    .transform(babelify)
-    .on('error', gutil.log)
-    .bundle()
-    .on('error', gutil.log)
-    .pipe(source('app.js'))
-    .pipe(gulp.dest('public'))
+gulp.task('start', ["fabrique", 'styles'], function() {
   console.log('Lancement du serveur');
   server.run(['serveur.js']);
+});
+
+// Tache pour controler l'execution de gulp dans Atom
+gulp.task('stop', function() {
+  //server.stop()
+  browserSync.exit();
 });
 
 gulp.task('default', ['dev']);
--- util/util.js
+++ util/util.js
@@ -2,13 +2,18 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://localhost:27017/test';
+console.log("Lancement de l'utilitaire: ")
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
-  rencontres.forEach((rencontre) => {
-    db.collection("rencontres").insert(rencontre)
-    console.log("Insertion rencontre: " + rencontre.id)
-  })
-  //db.close();
+  db.collection("rencontres").insertMany(rencontres, function(err, result) {
+    assert.equal(err, null);
+    console.log("Rencontres chargées.");
+    db.close();
+  });
+  // rencontres.forEach((rencontre) => {
+  //   db.collection("rencontres").insert(rencontre)
+  //   console.log("Insertion rencontre: " + rencontre.id)
+  // })
 })
 var rencontres = [{
   id: 1,
