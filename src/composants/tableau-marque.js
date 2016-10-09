a4ac865b64a4ae6c0bf001c93148437ae1567b3b
dahuchao <dahuchao.github@laposte.net>
2016-10-09 22:35:10 +0200

    Periodes

--- src/composants/tableau-marque.js
+++ src/composants/tableau-marque.js
@@ -4,6 +4,12 @@ export default React.createClass({
   render() {
     return (
       <div id="marque">
+        <div id="periode">
+          <span className="periode">T1</span>
+          <span className="periode">T2</span>
+          <span className="periode">T3</span>
+          <span className="periode">T4</span>
+        </div>
         <div className="compteur">
           <span className="hote"
             onClick={this.props.surCorrectionHote}>{this.props.hote}</span>-
