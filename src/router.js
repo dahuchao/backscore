4cc376a61c497d575b1ac96c1fdcfcf5d91b5f48
GRANDIERE Emmanuel <pwzf831@corp.log.intra.laposte.fr>
2016-10-10 14:37:44 +0200

    Suppression du paramètre _k

--- src/router.js
+++ src/router.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Router, IndexRedirect, Route, hashHistory } from 'react-router';
+import { Router, IndexRedirect, Route, hashHistory, useRouterHistory } from 'react-router';
 import AccueilConteneur from "./composants/accueil-conteneur.js"
 import RencontresConteneur from "./composants/rencontres-conteneur.js"
 import RencontreConteneur from "./composants/rencontre-conteneur.js"
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+import { createHashHistory } from 'history'
+
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
 
 export default (
   <MuiThemeProvider>
-    <Router >
+    <Router history={appHistory}>
       <Route path="/" component={AccueilConteneur} >
         <IndexRedirect to="/rencontres" />
         <Route path="/rencontres/:idRencontre" component={RencontreConteneur} />
